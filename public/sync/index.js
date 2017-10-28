a4ba9b4d04ccb47d1b6fc805f76e2ea26e199067
Qasim Abbas <qasimabbas52@gmail.com>
2017-10-28 18:13:25 -0400

    Clean up all the garbage

--- public/sync/index.js
+++ public/sync/index.js
@@ -3,7 +3,7 @@ $(function () {
   var $message = $('#message');
 
   //Get handle to the game board buttons
-  var $buttons = $('#board .board-row button');
+  //var $buttons = $('#board .board-row button');
 
   //Our interface to the Sync service
   var syncClient;
@@ -24,83 +24,19 @@ $(function () {
     $message.html('Sync initialized!');
 
     //Now that Sync is active, lets enable our game board
-    $buttons.attr('disabled', false);
+    //$buttons.attr('disabled', false);
 
     //This code will create and/or open a Sync document
     //Note the use of promises
-    syncClient.document('SyncGame').then(function(doc) {
+    syncClient.document('SyncSong').then(function(doc) {
       //Lets store it in our global variable
       syncDoc = doc;
 
-      //Initialize game board UI to current state (if it exists)
-      var data = syncDoc.get();
-      if (data.board) {
-        updateUserInterface(data);
-      }
-
       //Let's subscribe to changes on this document, so when something
       //changes on this document, we can trigger our UI to update
-      syncDoc.on('updated', updateUserInterface);
 
     });
 
   });
 
-  //Whenever a board button is clicked:
-  $buttons.on('click', function (e) {
-    //Toggle the value: X, O, or empty
-    toggleCellValue($(e.target));
-
-    //Update the document
-    var data = readGameBoardFromUserInterface();
-
-    //Send updated document to Sync
-    //This should trigger "updated" events on other clients
-    syncDoc.set(data);
-
-  });
-
-  //Toggle the value: X, O, or empty (&nbsp; for UI)
-  function toggleCellValue($cell) {
-    var cellValue = $cell.html();
-
-    if (cellValue === 'X') {
-      $cell.html('O');
-    } else if (cellValue === 'O') {
-      $cell.html('&nbsp;');
-    } else {
-      $cell.html('X');
-    }
-  }
-
-  //Read the state of the UI and create a new document
-  function readGameBoardFromUserInterface() {
-    var board = [
-      ['', '', ''],
-      ['', '', ''],
-      ['', '', '']
-    ];
-
-    for (var row = 0; row < 3; row++) {
-      for (var col = 0; col < 3; col++) {
-        var selector = '[data-row="' + row + '"]' +
-        '[data-col="' + col + '"]';
-        board[row][col] = $(selector).html().replace('&nbsp;', '');
-      }
-    }
-
-    return {board: board};
-  }
-
-  //Update the buttons on the board to match our document
-  function updateUserInterface(data) {
-    for (var row = 0; row < 3; row++) {
-      for (var col = 0; col < 3; col++) {
-        var selector = '[data-row="' + row + '"]' +
-        '[data-col="' + col + '"]';
-        var cellValue = data.board[row][col];
-        $(selector).html(cellValue === '' ? '&nbsp;' : cellValue);
-      }
-    }
-  }
 });
